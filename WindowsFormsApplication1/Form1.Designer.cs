05a7e6a0bd775398d4487ac99e804330c476622c
Eraytann <111294190+Eraytann@users.noreply.github.com>
2023-04-18 21:44:17 +0300

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "1.png");
+            this.ımageList1.Images.SetKeyName(1, "2.png");
+            this.ımageList1.Images.SetKeyName(2, "3.png");
+            this.ımageList1.Images.SetKeyName(3, "4.png");
+            this.ımageList1.Images.SetKeyName(4, "5.png");
+            this.ımageList1.Images.SetKeyName(5, "6.png");
+            this.ımageList1.Images.SetKeyName(6, "bos.png");
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(12, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(250, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(22, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Resim eşleştirme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] dizi = new int[12];
+        PictureBox[] resim = new PictureBox[12];
+        int icerik,icerik1=-1,icerik2=-1;
+        int tiklananresim1, tiklananresim2;
+        int ars = 0;
+        int sure = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sayiuret();
+            pbox_olustur();
+            pbox_bosyukle();
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+        
+        private void sayiuret()
+        {
+            Random r = new Random();
+            int gecici;
+            for (int i = 0; i <= 11; i++)  // 12 elemanlı dizi içerisinde 0'dan 11'e kadar sayıları atıyor.
+            {
+               dizi[i] = -1;
+                do
+                {
+                    gecici = r.Next(0, 12);
+                } while (Array.IndexOf(dizi,gecici)!=-1);
+                dizi[i] = gecici;
+            }
+            for (int i = 0; i <=11; i++)
+            {
+                if (dizi[i]>5)
+                {
+                    dizi[i] = dizi[i] % 6;
+                }
+            }
+           
+            
+        }
+        private void pbox_olustur()
+        {
+            int x =0;
+            int y = 30;
+            for (int i = 0; i < 12; i++)
+            {
+                if (i > 1 && i % 3 == 0) // her 3 pb da 1 alt satıra inme 
+                {
+                    y += 50; // pb ı alt satıra indirme
+                    x = 0;  // pb ı en sağa aldık
+                }
+                x += 50;    // pb ı sağa kaydırdık
+                resim[i] = new PictureBox();
+                resim[i].Location = new Point(x, y);     // pb ı form üzerinde konumlandırma
+                resim[i].Size = new Size(40, 40);       //  pb ın boyutu ayarlama
+                resim[i].Click += new EventHandler(tikla);  // pb a click özelliği kazandırma
+                resim[i].Tag = i;                           // hangi pb a tıklanıldığını öğrenmek için
+                this.Controls.Add(resim[i]);                // pb a form üzerine ekleme
+                resim[i].BorderStyle = BorderStyle.FixedSingle; // pb a çerceve ekleme
+            }
+        }
+        private void pbox_bosyukle()    // dinamik olarak oluşturulan pb dizisine boş resimler yükleme
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                resim[i].Image = ımageList1.Images[6];
+            }
+        }
+        private void tikla(object sender, EventArgs y) // pb nesnesinin olayı
+        {
+            icerik =dizi[Convert.ToInt16(((PictureBox)sender).Tag)];
+            if (icerik1==-1)   // ilk tıklanan resmi aç
+            {
+                icerik1 = icerik;
+                tiklananresim1=Convert.ToInt16(((PictureBox)sender).Tag);
+                resim[tiklananresim1].Image=ımageList1.Images[icerik1];
+                return;
+            }
+            if (icerik1!=-1 && icerik2==-1) // 2 . tıklanan resmi aç
+            {
+                tiklananresim2=Convert.ToInt16(((PictureBox)sender).Tag);
+                if (tiklananresim1!=tiklananresim2)
+                {
+                    icerik2 = icerik;
+                    resim[tiklananresim2].Image = ımageList1.Images[icerik2];
+                    this.Refresh();
+                }
+            }
+            if (icerik1!=-1 && icerik2!=-1)
+            {
+                if (icerik1==icerik2)
+                {
+                    //MessageBox.Show("BİLDİNİZ");
+                    ars++;
+                    icerik1 = -1;
+                    icerik2 = -1;
+                    resim[tiklananresim1].Visible = false;
+                    resim[tiklananresim2].Visible = false;
+                    if (ars == 6)
+                    {
+                        timer1.Stop();
+                        MessageBox.Show("SÜRE:"+sure.ToString(),"OYUN BİTTİ");
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    //MessageBox.Show("BİLEMEDİNİZ");
+                    icerik1 = -1;
+                    icerik2 = -1;
+                    System.Threading.Thread.Sleep(500);
+                    resim[tiklananresim1].Image = ımageList1.Images[6];
+                    resim[tiklananresim2].Image = ımageList1.Images[6];
+                }
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sure++;
+            label1.Text = sure.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sure += 3;
+            for (int i = 0; i < 12; i++)
+            {
+                resim[i].Image = ımageList1.Images[dizi[i]];
+            }
+            this.Refresh();
+            System.Threading.Thread.Sleep(1000);
+            for (int i = 0; i < 12; i++)
+            {
+                resim[i].Image = ımageList1.Images[6];
+            }
+            button1.Enabled = false;
+        }
+
+    }
+}
